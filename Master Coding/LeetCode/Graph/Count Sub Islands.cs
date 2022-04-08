a4d08bafac898c004cfdb61427e2e6ebc3de1ae3
Richkazz <edawarekaro@gmail.com>
2022-04-08 22:08:29 +0100

    Count_Sub_Islands leetcode

---
+++ Master Coding/LeetCode/Graph/Count Sub Islands.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_Coding.LeetCode.Graph
+{
+    public class Count_Sub_Islands
+    {
+        int total = 0;
+        bool check = true;
+        public int CountSubIslands(int[][] grid1, int[][] grid2)
+        {
+            for (var i = 0; i < grid2.Length; i++)
+            {
+                for (var j = 0; j < grid2[0].Length; j++)
+                {
+                    if (grid2[i][j] == 1)
+                    {
+                        dfs(grid2, grid1, i, j);
+                        if (check)
+                        {
+                            total += 1;
+                        }
+                        check = true;
+
+                    }
+                }
+            }
+            return total;
+        }
+        void dfs(int[][] grid2, int[][] grid1, int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= grid2.Length || j >= grid2[0].Length || grid2[i][j] != 1)
+            {
+                return;
+            }
+            if (grid1[i][j] != 1)
+            {
+                check = false;
+            }
+            grid2[i][j] = 2;
+            if (check)
+            {
+                check = true;
+            }
+            dfs(grid2, grid1, i - 1, j);
+            dfs(grid2, grid1, i + 1, j);
+            dfs(grid2, grid1, i, j - 1);
+            dfs(grid2, grid1, i, j + 1);
+
+        }
+    }
+}
---
+++ Master Coding/LeetCode/Graph/Number of Enclaves.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_Coding.LeetCode.Graph
+{
+    public class Number_of_Enclaves
+    {
+        int count = 0;
+        int total = 0;
+        bool check = true;
+        public int NumEnclaves(int[][] grid)
+        {
+            for (var i = 0; i < grid.Length; i++)
+            {
+                for (var j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        dfs(grid, i, j);
+                        if (check)
+                        {
+                            total += count;
+                        }
+                        check = true;
+                        count = 0;
+                    }
+                }
+            }
+            return total;
+        }
+
+        void dfs(int[][] grid, int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= grid.Length || j >= grid[0].Length)
+            {
+                check = false;
+                return;
+            }
+            if (grid[i][j] == 0)
+            {
+                return;
+            }
+            grid[i][j] = 0;
+            count++;
+            if (check)
+            {
+                check = true;
+            }
+            dfs(grid, i - 1, j);
+            dfs(grid, i + 1, j);
+            dfs(grid, i, j - 1);
+            dfs(grid, i, j + 1);
+
+        }
+    }
+}
